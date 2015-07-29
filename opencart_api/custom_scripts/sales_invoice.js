4b8cfa5a4b0820ede7d2a6bd4ec71f88602bd0ad
olhonko <olhonko@gmail.com>
2015-07-29 19:26:45 +0300

    Provided section currency and price list without changes when orders pulled from oc

--- opencart_api/custom_scripts/sales_invoice.js
+++ opencart_api/custom_scripts/sales_invoice.js
@@ -1,28 +1,16 @@
 cur_frm.cscript.is_pos = function (doc, dt, dn, callback_fn) {
-	var me = this;
-	var initial_price_list = me.frm.doc.selling_price_list;
-	var initial_price_list_currency = me.frm.doc.price_list_currency;
-	var initial_plc_conversion_rate = me.frm.doc.plc_conversion_rate;
-	var initial_currency = me.frm.doc.currency;
-	var initial_conversion_rate = me.frm.doc.conversion_rate;
 	cur_frm.cscript.hide_fields(this.frm.doc);
 	if(cint(this.frm.doc.is_pos)) {
 		if(!this.frm.doc.company) {
 			this.frm.set_value("is_pos", 0);
 			msgprint(__("Please specify Company to proceed"));
 		} else {
+			var me = this;
 			return this.frm.call({
 				doc: me.frm.doc,
 				method: "set_missing_values",
 				callback: function(r) {
 					if(!r.exc) {
-						// if (me.frm.doc.selling_price_list != initial_price_list) {
-						// 	me.frm.doc.selling_price_list = initial_price_list;
-						// 	me.frm.doc.price_list_currency = initial_price_list_currency;
-						// 	me.frm.doc.plc_conversion_rate = initial_plc_conversion_rate;
-						// 	me.frm.doc.currency = initial_currency;
-						// 	me.frm.doc.conversion_rate = initial_conversion_rate;
-						// }
 						me.frm.script_manager.trigger("update_stock");
 						frappe.model.set_default_values(me.frm.doc);
 						me.set_dynamic_labels();
--- opencart_api/sales_invoice.py
+++ opencart_api/sales_invoice.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from frappe import _, msgprint, throw
+from frappe.utils import cint, cstr, flt
 from erpnext.accounts.party import get_party_account, get_due_date
 
 import frappe
@@ -44,6 +45,53 @@ setattr(SalesInvoice, 'set_missing_values', set_missing_values)
 
 
 @frappe.whitelist()
+def set_pos_fields(self, for_validate=False):
+        """Set retail related fields from POS Profiles"""
+        if cint(self.is_pos) != 1:
+            return
+
+        from erpnext.stock.get_item_details import get_pos_profiles_item_details, get_pos_profiles
+        pos = get_pos_profiles(self.company)
+
+        if self.selling_price_list:
+            pos.selling_price_list = self.selling_price_list
+            pos.currency = self.currency
+
+        if pos:
+            if not for_validate and not self.customer:
+                self.customer = pos.customer
+                # self.set_customer_defaults()
+
+            for fieldname in ('territory', 'naming_series', 'currency', 'taxes_and_charges', 'letter_head', 'tc_name',
+                'selling_price_list', 'company', 'select_print_heading', 'cash_bank_account',
+                'write_off_account', 'write_off_cost_center'):
+                    if (not for_validate) or (for_validate and not self.get(fieldname)):
+                        self.set(fieldname, pos.get(fieldname))
+
+            if not for_validate:
+                self.update_stock = cint(pos.get("update_stock"))
+
+            # set pos values in items
+            for item in self.get("items"):
+                if item.get('item_code'):
+                    for fname, val in get_pos_profiles_item_details(pos,
+                        frappe._dict(item.as_dict()), pos).items():
+
+                        if (not for_validate) or (for_validate and not item.get(fname)):
+                            item.set(fname, val)
+
+            # fetch terms
+            if self.tc_name and not self.terms:
+                self.terms = frappe.db.get_value("Terms and Conditions", self.tc_name, "terms")
+
+            # fetch charges
+            if self.taxes_and_charges and not len(self.get("taxes")):
+                self.set_taxes()
+
+setattr(SalesInvoice, 'set_pos_fields', set_pos_fields)
+
+
+@frappe.whitelist()
 def resolve_mode_of_payment(payment_method_code, territory=''):
     res_mode_of_payment = ''
     all_mode_of_payments = frappe.get_all('Mode of Payment', fields=['name', 'oc_territory'], filters={'oc_payment_method_code': payment_method_code})
@@ -65,17 +113,6 @@ def resolve_mode_of_payment(payment_method_code, territory=''):
 def on_submit(self, method=None):
     pass
 
-
-# @frappe.whitelist()
-# def set_missing_values(self, for_validate=False):
-#     raise Exception('sales_invoice')
-#     self.set_pos_fields(for_validate)
-#     if not self.debit_to:
-#         self.debit_to = get_party_account(self.company, self.customer, "Customer")
-#     if not self.due_date:
-#         self.due_date = get_due_date(self.posting_date, "Customer", self.customer, self.company)
-
-    #super(SalesInvoice, self).set_missing_values(for_validate)
 
     # from erpnext.selling.doctype.customer.customer import get_customer_outstanding
     # customer_outstanding = get_customer_outstanding(self.customer, self.company)
