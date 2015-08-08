09d176aa55627a2185b013c5e818735d47a725ad
olhonko <olhonko@gmail.com>
2015-08-08 23:40:40 +0300

    Fixed shipping rule in customer group

--- opencart_api/orders.py
+++ opencart_api/orders.py
@@ -980,9 +980,9 @@ def resolve_shipping_rule(customer, db_customer=None, doc_customer=None, doc_oc_
     oc_site = obj_customer.get('oc_site')
     customer_group = obj_customer.get('customer_group')
 
-    doc_customer_group = frappe.get_doc('Customer Group', customer_group)
-    if doc_customer_group.get('shipping_rule'):
-        return doc_customer_group.get('shipping_rule')
+    shipping_rule = frappe.db.get_value('Customer Group', customer_group, "shipping_rule")
+    if shipping_rule:
+        return shipping_rule
 
     if oc_site:
         # resolve shipping rule for customr from Opencart site
