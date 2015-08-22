aed209b7e3c2fb18d2400482bcd26b1dcd8104f2
olhonko <olhonko@gmail.com>
2015-08-22 19:03:54 +0300

    Make up for comments.

--- opencart_api/comments.py
+++ opencart_api/comments.py
@@ -3,9 +3,14 @@ from frappe import _
 
 import frappe
 
+COMMENT_ADDED_FROM_TEMPLATE = '''<br><small class="text-muted"><span>comment from %s</span></small>'''
+
 
 def before_save(doc, method=None):
-    if doc.comment_doctype in ("Sales Invoice", "Delivery Note", "Packing Slip"):
-        doc_comment_name = frappe.get_doc(_(doc.comment_doctype), doc.comment_docname)
-        doc_sales_order = frappe.get_doc('Sales Order', doc_comment_name.get('sales_order'))
-        doc_sales_order.add_comment(doc.comment_type, text=doc.comment + ' ' + doc.comment_docname, comment_by=doc.comment_by)
+    if doc.comment_doctype in ("Back Order", "Sales Invoice", "Delivery Note", "Packing Slip"):
+        doc_comment = frappe.get_doc(_(doc.comment_doctype), doc.comment_docname)
+        if frappe.db.exists('Sales Order', doc_comment.get('sales_order')):
+            doc_sales_order = frappe.get_doc('Sales Order', doc_comment.get('sales_order'))
+            comment = doc.comment + COMMENT_ADDED_FROM_TEMPLATE % doc.comment_docname
+            doc_sales_order.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by)
+            frappe.clear_cache()
--- opencart_api/communications.py
+++ opencart_api/communications.py
@@ -3,9 +3,14 @@ from frappe import _
 
 import frappe
 
+COMMENT_ADDED_FROM_TEMPLATE = '''<span class="text-muted">comment from %s</span>'''
+
 
 def before_save(doc, method=None):
-    if doc.reference_doctype in ("Sales Invoice", "Delivery Note", "Packing Slip"):
-        doc_reference_name = frappe.get_doc(_(doc.reference_doctype), doc.reference_name)
-        doc_sales_order = frappe.get_doc('Sales Order', doc_reference_name.get('sales_order'))
-        doc_sales_order.add_comment(doc.communication_medium, text=doc.content + doc.reference_name, comment_by=doc.user)
+    if doc.reference_doctype in ("Back Order", "Sales Invoice", "Delivery Note", "Packing Slip"):
+        doc_reference = frappe.get_doc(_(doc.reference_doctype), doc.reference_name)
+        if frappe.db.exists('Sales Order', doc_reference.get('sales_order')):
+            doc_sales_order = frappe.get_doc('Sales Order', doc_reference.get('sales_order'))
+            comment = doc.content + COMMENT_ADDED_FROM_TEMPLATE % doc.reference_name
+            doc_sales_order.add_comment(doc.communication_medium, text=comment, comment_by=doc.user)
+            frappe.clear_cache()
