8123b35fc77fb2911cad4eb0ea021100350bd4cf
olhonko <olhonko@gmail.com>
2015-09-04 23:35:48 +0300

    Fixed scanning ps items.

--- opencart_api/custom_scripts/packing_slip.js
+++ opencart_api/custom_scripts/packing_slip.js
@@ -5,7 +5,6 @@ cur_frm.cscript.on_submit = function(doc, сdt, сdn) {
     frappe.set_route("Form", "Delivery Note", doc.delivery_note);
 }
 
-
 cur_frm.cscript.validate_duplicate_items = function(doc, ps_detail) {
     for(var i=0; i<ps_detail.length; i++) {
         for(var j=0; j<ps_detail.length; j++) {
@@ -18,152 +17,11 @@ cur_frm.cscript.validate_duplicate_items = function(doc, ps_detail) {
     }
 }
 
-
-function print_scan_items_log() {
-    var $table = $('<table class="table table-hover" style="border: 1px solid #D1D8DD;"></table>');
-    var $th = $('<tr style="background-color: #F7FAFC; font-size: 85%; color: #8D99A6"></tr>');
-    var $tbody = $('<tbody id="scan-items-tbody" style="font-family: Helvetica Neue, Helvetica, Arial, "Open Sans", sans-serif;font-size: 11.9px; line-height: 17px; color: #8D99A6;  background-color: #fff;"></tbody>');
-    $th.html('<th>Item Code</th><th>Barcode</th><th>Quantity</th><th>Status</th>');
-
-    for(var i=0; i < cur_frm.cscript.scanned_items.length; i++) {
-        $tr = $('<tr>');
-        $tr.append('<td>' + cur_frm.cscript.scanned_items[i].item_code + '</td>');
-        $tr.append('<td>' + cur_frm.cscript.scanned_items[i].barcode + '</td>');
-        $tr.append('<td>' + cur_frm.cscript.scanned_items[i].quantity + '</td>');
-        $tr.append('<td>' + cur_frm.cscript.scanned_items[i].status + '</td>');
-        $tbody.append($tr);
-    }
-
-    $table.append($th).append($tbody);
-    var $panel = $('<div class="panel"></div>');
-
-    $panel.append($table);
-    var msg = $('<div>').append($panel).html();
-    $(cur_frm.fields_dict['scan_items_log'].wrapper).html(msg);
-}
-
-
-frappe.scan_prompt = function(fields, callback, title, primary_label) {
-    if(!$.isArray(fields)) fields = [fields];
-    var d = new frappe.ui.Dialog({
-        fields: fields,
-        title: title || __("Enter Value"),
-    })
-    d.set_primary_action(primary_label || __("Submit"), function() {
-        var values = d.get_values();
-        if(!values) {
-            return;
-        }
-        d.hide();
-        callback(values);
-    })
-
-    d.$wrapper.on("shown.bs.modal", function() {
-        d.$wrapper.find(".modal-body :input:first").get(0).focus();
-    })
-    d.show();
-    return d;
-}
-
-
 cur_frm.cscript.custom_refresh = function(doc, dt, dn) {
     if(!doc.__islocal && doc.docstatus == 0) {
         cur_frm.cscript.setup_dashboard(doc);
     }
-    cur_frm.cscript.scanned_items = [];
-    cur_frm.cscript.items_to_scan = {};
-
-    if (doc.items) {
-        for(var i=0; i < doc.items.length; i++) {
-            cur_frm.cscript.items_to_scan[doc.items[i].item_code] = {
-                "item_code": doc.items[i].item_code,
-                "quantity": doc.items[i].qty,
-                "scanned_quantity": 0,
-                "barcode": ""
-            };
-        }
-    }
 }
-
-
-function on_item_scanned(item, scanned_quantity) {
-    if(cur_frm.cscript.items_to_scan[item.item_code]) {
-        var item_counting = cur_frm.cscript.items_to_scan[item.item_code]
-        
-        if(item_counting.scanned_quantity + scanned_quantity > item_counting.quantity) {
-            frappe.msgprint("Warning. It is already scanned " + item_counting.scanned_quantity + " of " + item_counting.quantity + " items (Item Code: " + item.item_code + ", Barcode: " + item.barcode + ").");
-        } else {
-            item_counting.scanned_quantity += scanned_quantity;
-            item_counting["barcode"] = item.barcode;
-            cur_frm.cscript.scanned_items.push({
-                "item_code": item.item_code,
-                "barcode": item.barcode,
-                "quantity": scanned_quantity,
-                "status": 'Scanned'
-            });
-            print_scan_items_log();
-        }
-    }
-    else {
-        frappe.msgprint("Warning. Item not found it the item list (Item Code: " + item.item_code + ", Barcode: " + item.barcode + ").");
-    }
-}
-
-
-function scan_items(frm) {
-    if (frm.doc.items) {
-        print_scan_items_log();
-        frappe.scan_prompt([{label:"Barcode", fieldtype:"Data", reqd: 1},
-                            {fieldtype:"Column Break"},
-                            {label:"Quantity", fieldtype:"Float", reqd: 1, default: 1}
-                           ],
-            function(data) {
-                frappe.call({
-                    method:"opencart_api.packing_slip.get_item_by_barcode",
-                    args: {
-                        barcode: data.barcode
-                    },
-                    callback: function(r) {
-                        if(!r.exc) {
-                            if(r.message) {
-                                on_item_scanned(r.message, data.quantity);
-                            } else {
-                                show_alert("Could not found Item with barcode " + data.barcode, 2);
-                            }
-                        }
-                        scan_items(frm);
-                    }
-                });
-            }
-        , __("Scan Items"), __("Proceed"));
-    } else {
-        frappe.msgprint('You cannot scan items for empty item list.');
-    }
-}
-
-frappe.ui.form.on("Packing Slip", "scan_items", scan_items);
-
-
-frappe.ui.form.on("Packing Slip", "check_scanned_items", function(frm) {
-    var is_all_ok = true;
-    for (var key in cur_frm.cscript.items_to_scan) {
-        var item_counting = cur_frm.cscript.items_to_scan[key];
-        if(item_counting.scanned_quantity == 0) {
-            is_all_ok = false;
-            frappe.msgprint("None of " + item_counting.quantity + " items were scanned (Item Code: " + item_counting.item_code + ", Barcode: " + item_counting.barcode + ").");
-        } else if(item_counting.scanned_quantity < item_counting.quantity) {
-            is_all_ok = false;
-            frappe.msgprint("Only " + item_counting.scanned_quantity + " of " + item_counting.quantity + " items were scanned (Item Code: " + item_counting.item_code + ", Barcode: " + item_counting.barcode + ").");
-        } else if(item_counting.scanned_quantity > item_counting.quantity) {
-            is_all_ok = false;
-            frappe.msgprint("Error. Scanned items count " + item_counting.scanned_quantity + " exceeds the required item count " + item_counting.quantity + ".");
-        }
-
-    }
-    if(is_all_ok) {
-        frappe.msgprint("All scanned items are matched.");
-    }
-});
 
 cur_frm.cscript.setup_dashboard = function(doc) {
     cur_frm.dashboard.reset(doc);
@@ -191,4 +49,282 @@ cur_frm.dashboard.add_badge = function(label, no, doctype, onclick) {
         this.wrapper.toggle(true);
 
         return badge.find(".alert-badge");
+}
+
+frappe.scan_prompt_dialog1 = function(callback, notification) {
+    if (notification) {
+        var label = "";
+        if (notification[0] == "danger") {
+            label = "<h4><span class=\"label label-danger\">" + notification[1] + "</span></h4>";
+        } else if (notification[0] == "success") {
+            label = "<h4><span class=\"label label-success\">" + notification[1] + "</span></h4>";
+        } else {
+            label = "<h4><span class=\"label label-info\">" + notification[1] + "</span></h4>";
+        }
+
+        var fields = [
+            {
+                fieldname:"msg",
+                fieldtype:"HTML",
+                options: "<div style=\"margin: 0px\" class=\"form-group\">" +
+                            label +
+                        "</div>"
+            },
+            {fieldtype:"Section Break"},
+            {label:"Barcode", fieldtype:"Data", reqd: 1}
+        ];
+    } else {
+        var fields = [
+            {label:"Barcode", fieldtype:"Data", reqd: 1}
+        ];
+    }
+    var d = new frappe.ui.Dialog({
+        fields: fields,
+        title: __("Scan Items"),
+    })
+    d.set_primary_action(__("Proceed"), function() {
+        var values = d.get_values();
+        if(!values) {
+            return;
+        }
+        d.hide();
+        callback(values);
+    })
+
+    d.$wrapper.on("shown.bs.modal", function() {
+        d.$wrapper.find(".modal-body :input:first").get(0).focus();
+    })
+    d.show();
+}
+
+frappe.scan_prompt_dialog2 = function(barcode, item, callback) {
+    var fields = [
+        {
+            fieldname:"item_description",
+            fieldtype:"HTML",
+            options: "<div style=\"margin: 0px\" class=\"form-group\">" +
+                        "<label style=\"padding-right: 0px;\" class=\"control-label\">Item Code</label>" +
+                        "<div class=\"control-input-wrapper\">" +
+                            "<div style=\"\" class=\"control-value like-disabled-input\">" + item.item_code + "</div>" +
+                        "</div>" +
+                    "</div>" +
+                    "<div style=\"margin: 0px\" class=\"form-group\">" +
+                        "<label style=\"padding-right: 0px;\" class=\"control-label\">Item Name</label>" +
+                        "<div class=\"control-input-wrapper\">" +
+                            "<div style=\"\" class=\"control-value like-disabled-input\">" + item.item_name + "</div>" +
+                        "</div>" +
+                    "</div>"
+        },
+        {fieldtype:"Column Break"},
+        {
+            fieldname:"image",
+            fieldtype:"HTML",
+            options: "<img src='" + item.image + "' class='img-responsive'>"
+        },
+        {fieldtype:"Section Break"},
+        {
+            fieldname:"barcode",
+            fieldtype:"HTML",
+            options: "<div style=\"margin: 0px\" class=\"form-group\">" +
+                        "<label style=\"padding-right: 0px;\" class=\"control-label\">Barcode</label>" +
+                        "<div class=\"control-input-wrapper\">" +
+                            "<div style=\"\" class=\"control-value like-disabled-input\">" + barcode + "</div>" +
+                        "</div>" +
+                    "</div>"
+        },
+        {fieldtype:"Column Break"},
+        {
+            label:"Quantity",
+            fieldname: "qty",
+            fieldtype:"Float",
+            reqd: 1,
+            default: 1
+        }
+    ];
+    var d = new frappe.ui.Dialog({
+        fields: fields,
+        title: __("Scan Items"),
+    })
+    d.set_primary_action(__("Proceed"), function() {
+        var values = d.get_values();
+        if(!values) {
+            return;
+        }
+        d.hide();
+        // patch values from dialog
+        values.barcode = barcode;
+        callback(values);
+    })
+
+    d.$wrapper.on("shown.bs.modal", function() {
+        d.$wrapper.find(".modal-body :input:last").get(0).focus();
+    })
+    d.show();
+}
+
+frappe.scan_prompt = function(barcode, item, callback, notification) {
+    // initial dialog to enter barcode and quantity
+    if (barcode == undefined && item == undefined) {
+        frappe.scan_prompt_dialog1(callback, notification);
+        return;
+    }
+    if (barcode) {
+        if (item) {
+            frappe.scan_prompt_dialog2(barcode, item, callback);
+        } else {
+            frappe.scan_prompt_dialog1(callback, notification);
+        }
+    } else {
+        frappe.scan_prompt_dialog1(callback, notification);
+    }
+}
+
+// ---
+//             var d = new frappe.ui.Dialog({
+//                 title: __("Upload User Permissions"),
+//                 fields: [
+//                     {
+//                         fieldtype:"HTML",
+//                         options: '<p class="text-muted"><ol>'+
+//                             "<li>"+__("Upload CSV file containing all user permissions in the same format as Download.")+"</li>"+
+//                             "<li><strong>"+__("Any existing permission will be deleted / overwritten.")+"</strong></li>"+
+//                         '</p>'
+//                     },
+//                     {
+//                         fieldtype:"Attach", fieldname:"attach",
+//                     }
+//                 ],
+//                 primary_action_label: __("Upload and Sync"),
+//                 primary_action: function() {
+//                     frappe.call({
+//                         method:"frappe.core.page.user_permissions.user_permissions.import_user_permissions",
+//                         args: {
+//                             filedata: d.fields_dict.attach.get_value()
+//                         },
+//                         callback: function(r) {
+//                             if(!r.exc) {
+//                                 msgprint(__("Permissions Updated"));
+//                                 d.hide();
+//                             }
+//                         }
+//                     });
+//                 }
+//             });
+//             d.show();
+
+
+// --------------
+// var d = new frappe.ui.Dialog({
+//                 title: "Workflow: "
+//                     + frappe.workflow.workflows[me.frm.doctype].name
+//             })
+//             var next_html = $.map(frappe.workflow.get_transitions(me.frm.doctype, state),
+//                 function(d) {
+//                     return d.action.bold() + __(" by Role ") + d.allowed;
+//                 }).join(", ") || __("None: End of Workflow").bold();
+
+//             $(d.body).html("<p>"+__("Current status")+": " + state.bold() + "</p>"
+//                 + "<p>"+__("Document is only editable by users of role")+": "
+//                     + frappe.workflow.get_document_state(me.frm.doctype,
+//                         state).allow_edit.bold() + "</p>"
+//                 + "<p>"+__("Next actions")+": "+ next_html +"</p>"
+//                 + (me.frm.doc.__islocal ? ("<div class='alert alert-info'>"
+//                     +__("Workflow will start after saving.")+"</div>") : "")
+//                 + "<p class='help'>"+__("Note: Other permission rules may also apply")+"</p>"
+//                 ).css({padding: '15px'});
+//             d.show();
+// ==================
+
+
+function scan_items(frm, cdt, cdn, barcode, item, callback, notification) {
+    if (frm.doc.items) {
+        check_scanned_items(frm);
+        if (callback == undefined) {
+            callback = function(data1) {
+                frappe.call({
+                    method:"opencart_api.packing_slip.get_item_by_barcode",
+                    args: {
+                        barcode: data1.barcode
+                    },
+                    callback: function(r) {
+                        if(!r.exc) {
+                            if(r.message) {
+                                var item = r.message;
+                                scan_items(frm, cdt, cdn, data1.barcode, item, function(data2) {
+                                    var items = frm.doc.items || [];
+                                    var item_found = false;
+                                    for(var i = 0; i < items.length; i++) {
+                                        if (items[i].item_code == item.item_code) {
+                                            item_found = true;
+                                            if (!items[i].scanned_qty) {
+                                                items[i].scanned_qty = 0;
+                                            }
+                                            if (items[i].scanned_qty + data2.qty > items[i].qty) {
+                                                var exceed_qty = items[i].scanned_qty + data2.qty - items[i].qty;
+                                                scan_items(frm, cdt, cdn, data1.barcode, undefined, undefined, ["danger", "Not added: total quantity of scanned items will exceed the required quantity more then " + exceed_qty + " items"]);
+                                                break;
+                                            }
+                                            items[i].scanned_qty += data2.qty;
+                                            items[i].barcode = data2.barcode;
+                                            frm.save("Save", function() {
+                                                scan_items(frm, cdt, cdn, data1.barcode, undefined, undefined, ["success", "Last successful scanning: " + data2.qty + " " + item.item_code + " items"]);
+                                            });
+                                            break;
+                                        }
+                                    }
+                                    if (!item_found) {
+                                        scan_items(frm, cdt, cdn, data1.barcode, undefined, undefined, ["danger", "Not added: Item \"" + item.item_code + "\" not found in the Item list of this Packing Slip"]);
+                                    }
+                                });
+                            } else {
+                                scan_items(frm, cdt, cdn, data1.barcode, undefined, undefined, ["danger", "Item with barcode \"" + data1.barcode + "\" not found"]);
+                            }
+                        }
+                    }
+                });
+            }
+        }
+        frappe.scan_prompt(barcode, item, callback, notification);
+    } else {
+        frappe.msgprint('You cannot scan items for empty item list.');
+    }
+}
+
+frappe.ui.form.on("Packing Slip", "scan_items", scan_items);
+
+function check_scanned_items(frm) {
+    // alert("background-color:red");
+    // var d = locals[cdt][cdn];
+    var wrapper = $(cur_frm.fields_dict.items.wrapper);
+    var items = frm.doc.items || [];
+    for(var i = 0; i < items.length; i++){
+        // items[i].scanned_qty = 0.0;
+        var row_wrapper = wrapper.find("[data-idx='"+ items[i].idx +"']").data("grid_row").wrapper;
+        if(row_wrapper) {
+            if (!items[i].scanned_qty || items[i].scanned_qty == 0) {
+                // red
+                row_wrapper.css('background-color', '#ff5858');
+            } else if(items[i].scanned_qty && items[i].scanned_qty < items[i].qty) {
+                // orange
+                row_wrapper.css('background-color', '#ffa00a');
+            } else if(items[i].scanned_qty && items[i].scanned_qty == items[i].qty) {
+                // green
+                row_wrapper.css('background-color', '#98d85b');
+            }
+        }
+    }
+}
+
+frappe.ui.form.on("Packing Slip", "check_scanned_items", function(frm) {
+    check_scanned_items(frm);
+});
+
+cur_frm.cscript.reset_scanned_items = function(doc) {
+    // alert("background-color:red");
+    // var d = locals[cdt][cdn];
+    var items = doc.items || [];
+    for(var i = 0; i < items.length; i++){
+        items[i].scanned_qty = 0.0;
+    }
+    refresh_field('items');
 }
