060cff58c67409fe2d01b957699ef0ac92724eb4
olhonko <olhonko@gmail.com>
2015-11-17 03:34:33 +0200

    Fixed assigning docs.

--- opencart_api/comments.py
+++ opencart_api/comments.py
@@ -7,11 +7,11 @@ COMMENT_ADDED_FROM_TEMPLATE = '''<br><small class="text-muted"><span>comment fro
 
 
 def before_save(doc, method=None):
-    if not (doc and doc.comment_doctype and doc.comment_docname):
+    if not (doc.comment_doctype in ('Sales Order', 'Sales Invoice', 'Packing Slip', 'Delivery Note') and doc.is_origin_comment):
         return
     doc_comment = frappe.get_doc(_(doc.comment_doctype), doc.comment_docname)
     comment = doc.comment + COMMENT_ADDED_FROM_TEMPLATE % doc.comment_docname
-    if doc.comment_doctype == 'Sales Order' and doc.is_origin_comment:
+    if doc.comment_doctype == 'Sales Order':
         ps = frappe.get_all('Packing Slip', filters={'sales_order': doc_comment.name})
         for i in ps:
             doc_packing_slip = frappe.get_doc('Packing Slip', i)
@@ -26,11 +26,11 @@ def before_save(doc, method=None):
             doc_sales_invoice.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by, is_origin_comment=0)
         frappe.clear_cache(doctype='Sales Order')
 
-    elif doc.comment_doctype == 'Packing Slip' and doc_comment.sales_order and doc.is_origin_comment:
+    elif doc.comment_doctype == 'Packing Slip' and doc_comment.sales_order:
         si = frappe.get_all('Sales Invoice', filters={'sales_order': doc_comment.sales_order})
         for i in si:
             doc_sales_invoice = frappe.get_doc('Sales Invoice', i)
-            doc_sales_invoice.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by,is_origin_comment=0)
+            doc_sales_invoice.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by, is_origin_comment=0)
         dn = frappe.get_all('Delivery Note', filters={'sales_order': doc_comment.sales_order})
         for i in dn:
             doc_delivery_note = frappe.get_doc('Delivery Note', i)
@@ -39,7 +39,7 @@ def before_save(doc, method=None):
         doc_sales_order.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by, is_origin_comment=0)
         frappe.clear_cache(doctype='Packing Slip')
 
-    elif doc.comment_doctype == 'Sales Invoice' and doc_comment.sales_order and doc.is_origin_comment:
+    elif doc.comment_doctype == 'Sales Invoice' and doc_comment.sales_order:
         ps = frappe.get_all('Packing Slip', filters={'sales_order': doc_comment.sales_order})
         for i in ps:
             doc_packing_slip = frappe.get_doc('Packing Slip', i)
@@ -52,7 +52,7 @@ def before_save(doc, method=None):
         doc_sales_order.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by, is_origin_comment=0)
         frappe.clear_cache(doctype='Sales Invoice')
 
-    elif doc.comment_doctype == 'Delivery Note' and doc_comment.sales_order and doc.is_origin_comment:
+    elif doc.comment_doctype == 'Delivery Note' and doc_comment.sales_order:
         ps = frappe.get_all('Packing Slip', filters={'sales_order': doc_comment.sales_order})
         for i in ps:
             doc_packing_slip = frappe.get_doc('Packing Slip', i)
@@ -60,7 +60,7 @@ def before_save(doc, method=None):
         si = frappe.get_all('Sales Invoice', filters={'sales_order': doc_comment.sales_order})
         for i in si:
             doc_sales_invoice = frappe.get_doc('Sales Invoice', i)
-            doc_sales_invoice.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by,is_origin_comment=0)
+            doc_sales_invoice.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by, is_origin_comment=0)
         doc_sales_order = frappe.get_doc('Sales Order', doc_comment.sales_order)
         doc_sales_order.add_comment(doc.comment_type, text=comment, comment_by=doc.comment_by, is_origin_comment=0)
         frappe.clear_cache(doctype='Delivery Note')
