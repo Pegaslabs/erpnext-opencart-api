dd321dd5dbab3f6fb7a38f9af6518a91b824e48c
olhonko <olhonko@gmail.com>
2015-11-24 14:28:21 +0200

    Improved communications.

--- opencart_api/communications.py
+++ opencart_api/communications.py
@@ -7,10 +7,26 @@ COMMENT_ADDED_FROM_TEMPLATE = '''<span class="text-muted">comment from %s</span>
 
 
 def before_save(doc, method=None):
-    if doc.reference_doctype in ("Back Order", "Sales Invoice", "Delivery Note", "Packing Slip"):
-        doc_reference = frappe.get_doc(_(doc.reference_doctype), doc.reference_name)
-        if frappe.db.exists('Sales Order', doc_reference.get('sales_order')):
-            doc_sales_order = frappe.get_doc('Sales Order', doc_reference.get('sales_order'))
-            comment = doc.content + COMMENT_ADDED_FROM_TEMPLATE % doc.reference_name
-            doc_sales_order.add_comment(doc.communication_medium, text=comment, comment_by=doc.user)
-            frappe.clear_cache()
+    if doc.reference_doctype in ("Sales Order", "Sales Invoice", "Delivery Note", "Packing Slip"):
+        doc_comment = frappe.get_doc(_(doc.reference_doctype), doc.reference_name)
+        comment = doc.content + COMMENT_ADDED_FROM_TEMPLATE % doc.reference_name
+        if not doc_comment.get('sales_order'):
+            if doc.reference_doctype == "Sales Order":
+                for doctype in ("Sales Invoice", "Delivery Note", "Packing Slip"):
+                    all_doctypes = frappe.get_all(doctype, filters={'sales_order': doc_comment.name})
+                    for i in all_doctypes:
+                        doc_reference = frappe.get_doc(doctype, i)
+                        doc_reference.add_comment(doc.communication_medium, text=comment, comment_by=doc.user, is_origin_comment=0)
+                    frappe.clear_cache(doctype=doctype)
+        else:
+            if frappe.db.exists('Sales Order', doc_comment.get('sales_order')):
+                doc_sales_order = frappe.get_doc('Sales Order', doc_comment.get('sales_order'))
+                doc_sales_order.add_comment(doc.communication_medium, text=comment, comment_by=doc.user, is_origin_comment=0)
+                frappe.clear_cache(doctype="Sales Order")
+                for doctype in ("Sales Invoice", "Delivery Note", "Packing Slip"):
+                    all_doctypes = frappe.get_all(doctype, filters={'sales_order': doc_comment.get('sales_order')})
+                    for i in all_doctypes:
+                        if i.get('name') != doc_comment.name:
+                            doc_comment_new = frappe.get_doc(doctype, i)
+                            doc_comment_new.add_comment(doc.communication_medium, text=comment, comment_by=doc.user, is_origin_comment=0)
+                    frappe.clear_cache(doctype=doctype)
