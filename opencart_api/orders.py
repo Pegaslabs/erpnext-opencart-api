6b96d78ed43cf7e2850f25bf09e6a43e60e14186
olhonko <olhonko@gmail.com>
2016-05-09 18:21:03 +0300

    Using first and last names from Credit Card document.

--- opencart_api/orders.py
+++ opencart_api/orders.py
@@ -101,12 +101,17 @@ def on_submit(doc, method=None):
             si.submit()
 
             from erpnext.accounts.doctype.journal_entry.journal_entry import get_cc_payment_entry_against_invoice, get_payment_entry_against_invoice, add_converge_transaction
+            from erpnext.selling.doctype.recurring_profile.recurring_profile import get_credit_card_or_throw
             if is_converge:
                 je = frappe.get_doc(get_cc_payment_entry_against_invoice(si.doctype, si.name, is_recurring=True))
                 je.posting_date = doc.transaction_date
                 je.insert()
 
-                tr = add_converge_transaction(je.name, si, transaction_args=recurring_profile_doc.as_dict(), transaction_id=recurring_profile_doc.initial_transaction_id)
+                transaction_args = {
+                    "rp": recurring_profile_doc.as_dict(),
+                    "cc": get_credit_card_or_throw(recurring_profile_doc.cc_token_id)
+                }
+                tr = add_converge_transaction(je.name, si, transaction_args=transaction_args, transaction_id=recurring_profile_doc.initial_transaction_id)
                 tr.submit()
                 je.submit()
 
