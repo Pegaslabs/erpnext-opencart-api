5651ff53f45513a9bcbc275b3928879f2d2383bc
olhonko <olhonko@gmail.com>
2016-11-17 18:08:21 +0200

    Updated desktop icons.

--- opencart_api/config/desktop.py
+++ opencart_api/config/desktop.py
@@ -2,11 +2,24 @@ from frappe import _
 
 
 def get_data():
-    return {
-        "Opencart API": {
-            "color": "#589494",
-            "icon": "icon-book",
-            "type": "module",
-            "label": _("Opencart API")
+    return [
+        {
+            "module_name": "Opencart API",
+            "label": _("Opencart API"),
+            "color": "#00004d",
+            "icon": "octicon octicon-git-compare",
+            "idx": 53,
+            "reverse": 0,
+            "type": "module"
+        },
+        {
+            "module_name": "Opencart Site",
+            "_doctype": "Opencart Site",
+            "color": "#00004d",
+            "icon": "octicon octicon-gist",
+            "idx": 53,
+            "reverse": 0,
+            "type": "link",
+            "link": "List/Opencart Site"
         }
-    }
+    ]
